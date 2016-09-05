c9ce2128d8c00775addce62a7972dbd866fe3908
Sergei Dolgov <dolgovs@gmail.com>
2016-09-05 22:17:10 +0200

    Load translation in an iframe

---
+++ src/data.js
@@ -0,0 +1,31 @@
+export const text =
+
+  `Il y avait en Vestphalie, dans le château de M. le baron de
+Thunder-ten-tronckh, un jeune garçon à qui la nature avait donné les mœurs les
+plus douces. Sa physionomie annonçait son âme. Il avait le jugement assez
+droit, avec l’esprit le plus simple ; c’est, je crois, pour cette raison qu’on
+le nommait Candide. Les anciens domestiques de la maison soupçonnaient qu’il
+était fils de la sœur de monsieur le baron et d’un bon et honnête gentilhomme
+du voisinage, que cette demoiselle ne voulut jamais épouser parce qu’il n’avait
+pu prouver que soixante et onze quartiers[1], et que le reste de son arbre
+généalogique avait été perdu par l’injure du temps.
+
+  Monsieur le baron était un des plus puissants seigneurs de la Vestphalie, car
+son château avait une porte et des fenêtres. Sa grande salle même était ornée
+d’une tapisserie. Tous les chiens de ses basses-cours composaient une meute
+dans le besoin ; ses palefreniers étaient ses piqueurs ; le vicaire du village
+était son grand aumônier. Ils l’appelaient tous Monseigneur, et ils riaient
+quand il faisait des contes.
+
+  Madame la baronne, qui pesait environ trois cent cinquante livres, s’attirait
+par là une très-grande considération, et faisait les honneurs de la maison avec
+une dignité qui la rendait encore plus respectable. Sa fille Cunégonde, âgée de
+dix-sept ans, était haute en couleur, fraîche, grasse, appétissante. Le fils du
+baron paraissait en tout digne de son père. Le précepteur Pangloss[2] était
+l’oracle de la maison, et le petit Candide écoutait ses leçons avec toute la
+bonne foi de son âge et de son caractère.
+
+  Pangloss enseignait la métaphysico-théologo-cosmolo-nigologie. Il prouvait
+admirablement qu’il n’y a point d’effet sans cause, et que, dans ce meilleur
+des mondes possibles, le château de monseigneur le baron était le plus beau des
+châteaux, et madame la meilleure des baronnes possibles.`
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,51 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-const Hello = React.createClass({
+import {text} from './data.js'
+
+const Word = ({word, onWordClick}) => {
+  const onClick = () => {
+    onWordClick(word);
+  }
+  return <span className="word" onClick={onClick}>{word}</span>;
+}
+
+const Phrase = ({content, onWordClick}) => {
+  const words = content.split(/[\ \n]+/);
+  return (
+    <p>
+      {words.map((w, i) => <Word key={i} word={w} onWordClick={onWordClick}/>)}
+    </p>);
+}
+
+const Text = ({text, onWordClick}) => {
+  const phrases = text.split(/\n{2,}/);
+  return (
+    <div className="text">
+      {phrases.map((s, i) => <Phrase content={s} key={i} onWordClick={onWordClick}/>)}
+    </div>);
+}
+
+const App = React.createClass({
+  getInitialState() {
+    return {word: ''}
+  },
+  selectWord(word) {
+    this.setState({word})
+  },
   render() {
-    return <div>Hello</div>;
+    return <div className="app">
+      <Text text={text} onWordClick={this.selectWord}/>
+      <Dictionary word={this.state.word}/>
+    </div>;
+
   }
 })
 
-ReactDom.render(<Hello/>, document.getElementById('app'));
+const Dictionary = ({word}) => {
+  return <div className="dictionary">
+    <iframe src={"http://www.wordreference.com/fren/" + word}></iframe>
+  </div>
+}
+
+ReactDom.render(<App />, document.getElementById('app'));
